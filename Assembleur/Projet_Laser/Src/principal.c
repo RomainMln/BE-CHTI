03f9a3148988c7afe47d5e0f5de42a2381ecd927
g_robert <g_robert@etud.insa-toulouse.fr>
2022-05-10 16:44:37 +0200

    projet fini

--- Assembleur/Projet_Laser/Src/principal.c
+++ Assembleur/Projet_Laser/Src/principal.c
@@ -1,7 +1,12 @@
 
 
-#include "DriverJeuLaser.h"
+#include "../Driver/DriverJeuLaser.h"
+#include "../Driver/Affichage_Valise.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+extern void Prepare_Afficheur(char Aff, char Valeur);
 extern int DFT_ModuleAuCarre( short int * , char );
 extern void CallbackSon(void);
 extern void LancerSon(void);
@@ -10,24 +15,48 @@ extern short LeSignal[];
 int test=0;
 int result[4];
 int score[4] = {0,0,0,0};
-int seuil = 5000000;
+int seuil = 1000000;
 short bon = 0;
+int random;
+
+void AfficheAll(){
+	int aff =0;
+	for(int k=1;k<=4;k++){
+		aff = score[k-1];
+		if(aff >99)
+			Prepare_Afficheur(k,99);
+		else
+			Prepare_Afficheur(k,aff);
+			Prepare_Clear_Point_Unite(k);
+	}
+	Prepare_Set_LED(random);
+	Choix_Capteur(random+1);
+	
+}
 
 short dma_buf[64];
 void callback(){
+	int mem ; 
 	short compt = 0;
 	Start_DMA1(64);
 	Wait_On_End_Of_DMA1();
 	Stop_DMA1;
-	result[0]=DFT_ModuleAuCarre(dma_buf,17)>>1;
-	result[1]=DFT_ModuleAuCarre(dma_buf,18)>>1;
-	result[2]=DFT_ModuleAuCarre(dma_buf,19)>>1;
-	result[3]=DFT_ModuleAuCarre(dma_buf,20)>>1;
+	result[0]=DFT_ModuleAuCarre(dma_buf,17);
+	result[1]=DFT_ModuleAuCarre(dma_buf,18);
+	result[2]=DFT_ModuleAuCarre(dma_buf,19);
+	result[3]=DFT_ModuleAuCarre(dma_buf,20);
 	for(int i=0;i<4;i++){
 		if(result[i]>seuil && bon==0){
 			score[i]++;
 			bon = 1;
+			//afficheurs(i+1,score[i]);
 			LancerSon();
+			Prepare_Clear_LED(random);
+			srand(rand()%RAND_MAX);
+			mem = random;
+			while(random == mem )
+				random = rand() % 4;
+			AfficheAll();
 		}
 		else if (result[i]<seuil ){
 			compt++;
@@ -36,8 +65,10 @@ void callback(){
 			}
 		}
 	}
+	
 }
 	
+
 int main(void)
 {
 
@@ -47,7 +78,6 @@ int main(void)
 
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
   CLOCK_Configure();
-	
 	
 	Systick_Period_ff(72000*5);
 	Systick_Prio_IT(2,callback);
@@ -63,12 +93,25 @@ int main(void)
 	GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
 	Timer_1234_Init_ff( TIM4,11000);
 	Active_IT_Debordement_Timer(TIM4,2,CallbackSon);
-
+	
+	srand(rand()%RAND_MAX);
+	random = rand() % 4;
+	Init_Affichage();
+	/*for(int k=1;k<=4;k++){
+		Prepare_Afficheur(k,score[k-1]);
+		Prepare_Clear_Point_Unite(k);
+	}
+	Prepare_Set_LED(LED_Cible_1);
+	Choix_Capteur(1);
+	Mise_A_Jour_Afficheurs_LED();
+	*/
+	
 //============================================================================	
-
+AfficheAll();
 
 while	(1)
 	{
+		Mise_A_Jour_Afficheurs_LED();
 	}
 }
 
